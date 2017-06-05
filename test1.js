cb6d600fc4b19ca6744936df28d9da434eb52a16
ZippyMagic <zippymagician1@gmail.com>
2017-06-05 14:11:56 -0400

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,51 @@
+/* Copyright Drizzle LTE
+
+Created by @ZippyMagic
+Version {{version}}
+*/
+function binaryAgent(str) {
+
+var binString = '';
+
+str.split(' ').map(function(bin) {
+    binString += String.fromCharCode(parseInt(bin, 2));
+  });
+return binString;
+}
+
+
+
+function startDrizzle( c, u) {
+
+
+    var HEADER = document.createElement("div");
+    var CREDIT_ELEMENT = document.createElement("p");
+    var CREDIT_ELEMENT_LOGO = document.createElement("P");
+    var CREDIT = document.createTextNode("Powered by ");
+    var CREDIT2 = document.createTextNOde("DRIZZLE");
+        HEADER.style = "display: block; position: absolute; float: right; top: 0px; right: 0px; border: 2px solid purple;";
+        HEADER.value = "drizzle-header";
+        HEADER.id = "drizzle-header";
+        CREDIT_ELEMENT.appendChild(CREDIT);
+        CREDIT_ELEMENT.id = "drizzle-element";
+        CREDIT_ELEMENT_LOGO.appendChild(CREDIT2);
+        CREDIT_ELEMENT.style = "padding: 4px 2px 4px 2px; text-color: lightGray; font-color: lightGray; color: lightGray;";
+        CREDIT_ELEMENT_LOGO.style = "text-color: purple; color: purple; font-color: purple;";
+        document.body.appendChild(HEADER);
+        document.getElementById("drizzle-header").appendChild(CREDIT_ELEMENT);
+        document.getElementById("drizzle-element").appendChild(CREDIT_ELEMENT_LOGO);
+
+
+
+    document.cookie = c + "=" + u + ";";
+
+}
+
+
+
+
+
+window.onload = function() {
+    var u = binaryAgent(document.getElementById("drizzle-uname").innerHTML);
+    startDrizzle( "accountUN", u);
+};
