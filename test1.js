b32d1d4a8a100156e445b69ef96eda208e830405
ZippyMagic <zippymagician1@gmail.com>
2017-06-05 14:40:08 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -2,9 +2,6 @@
 Created by @ZippyMagic
 Version {{version}}
 */
-
-
-
 function binaryAgent(str) {
 
 var binString = '';
@@ -22,27 +19,26 @@ return binString;
       
     var u = binaryAgent(document.getElementById("drizzle-uname").innerHTML);
     
-    
-       
+   
+
     var HEADER = document.createElement("DIV");
-    var CREDIT_ELEMENT = document.createElement("P");
-    var CREDIT_ELEMENT_LOGO = document.createElement("LOGO");
-    var CREDIT = document.createTextNode("Powered by ");
-    var CREDIT2 = document.createTextNOde("DRIZZLE");
+        HEADER.id = 'drizzle-header';
         HEADER.style = "display: block; position: absolute; float: right; top: 0px; right: 0px; border: 2px solid purple;";
-        HEADER.value = "drizzle-header";
-        HEADER.id = "drizzle-header";
-        CREDIT_ELEMENT.appendChild(CREDIT);
-        CREDIT_ELEMENT.id = "drizzle-element";
-        CREDIT_ELEMENT_LOGO.appendChild(CREDIT2);
+        document.body.appendChild(HEADER);
+    var CREDIT_ELEMENT = document.createElement("P");
+    var CREDIT = document.createTextNode("Powered by ");
+        CREDIT_ELEMENT.id= 'drizzle-element';
         CREDIT_ELEMENT.style = "padding: 4px 2px 4px 2px; text-color: lightGray; font-color: lightGray; color: lightGray;";
-        CREDIT_ELEMENT_LOGO.style = "text-color: purple; color: purple; font-color: purple;";
-        document.getElementById("body").appendChild(HEADER);
+        CREDIT_ELEMENT.appendChild(CREDIT);
         document.getElementById("drizzle-header").appendChild(CREDIT_ELEMENT);
+    var CREDIT_ELEMENT_LOGO = document.createElement("CODE");
+    var CREDIT2 = document.createTextNode("DRIZZLE");
+        CREDIT_ELEMENT_LOGO.style = "text-color: purple; color: purple; font-color: purple;";
+        CREDIT_ELEMENT_LOGO.appendChild(CREDIT2);
         document.getElementById("drizzle-element").appendChild(CREDIT_ELEMENT_LOGO);
-
-
-
+    
+    
+    
     document.cookie = "uName=" + u + ";";
 
     
