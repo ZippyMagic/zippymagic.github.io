c33f7b6b409bf2283e3314471c3b393a717e4f3d
ZippyMagic <zippymagician1@gmail.com>
2017-06-07 10:27:00 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -16,14 +16,23 @@ return binString;
 (function () {
 
 
-      
+    // Create Username Cookie
     var u = binaryAgent(document.getElementById("drizzle-uname").innerHTML);
-    
-   
+    document.cookie = "uName=" + u + ";";
 
+    
+    // Begin Style loader
+    var STYLE = document.createElement("LINK");
+        STYLE.rel = 'stylesheet';
+        STYLE.type = 'text/css';
+        STYLE.href = 'drizzle/drizzle-style.min.css'
+    document.getElementById("body").appendChild(STYLE);
+    
+    
+    // Begin Header Creation
     var HEADER = document.createElement("DIV");
         HEADER.id = 'drizzle-header';
-        HEADER.style = "display: block; position: absolute; float: right; top: 0px; right: 0px; border: 1px solid purple;";
+        HEADER.style = "display: block; position: absolute; float: right; top: 0px; right: 0px; border: 2px solid purple;";
         document.body.appendChild(HEADER);
     var CREDIT_ELEMENT = document.createElement("P");
     var CREDIT = document.createTextNode("Powered by ");
@@ -37,10 +46,22 @@ return binString;
         CREDIT_ELEMENT_LOGO.appendChild(CREDIT2);
         document.getElementById("drizzle-element").appendChild(CREDIT_ELEMENT_LOGO);
     
+    // Begin Website
+    var WEB_JSON = [
+        document.getElementById("drizzle-web-top-array").innerHTML
+    ]
+     
+    function createHeader(arr) {
+        var out = '<div id="drizzle-web-header">';
+        var count;
+        for(count = 0; count < arr.length; count++) {
+            out += '<a id="drizzle-web-header-content" href="' + arr[count].url + '">' + arr[count].display + '</a>';
+        }
+        var out2 = out;
+        var out = out2 + '</div>'
+        var doc = document.getElementById("body").innerHTML;
+            document.getElementById("body").innerHTML = doc + out;
+    }
     
-    
-    document.cookie = "uName=" + u + ";";
-
-    
-    
+    createHeader(WEB_JSON);
 })();
